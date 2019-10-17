f5480dc3b4d1b20066f7008aa88e4ac26b069e24
Gethi <bigard.antoine@gmail.com>
2019-10-17 14:12:41 -0500

    wip tmp

--- setup.sh
+++ setup.sh
@@ -6,29 +6,29 @@ pwd
 ls -l 
 ls -l /
 
-yum -y install gcc-c++ make git
-curl -sL https://rpm.nodesource.com/setup_10.x | bash -
-yum -y install nodejs
-curl -sL https://dl.yarnpkg.com/rpm/yarn.repo | tee /etc/yum.repos.d/yarn.repo
-rpm --import https://dl.yarnpkg.com/rpm/pubkey.gpg
-yum -y install yarn
+#yum -y install gcc-c++ make git
+#curl -sL https://rpm.nodesource.com/setup_10.x | bash -
+#yum -y install nodejs
+#curl -sL https://dl.yarnpkg.com/rpm/yarn.repo | tee /etc/yum.repos.d/yarn.repo
+#rpm --import https://dl.yarnpkg.com/rpm/pubkey.gpg
+#yum -y install yarn
 
-cd /wks
-git clone -b feature/refactor https://github.com/Gethi/discogs-client.git tmp
-mv tmp/* tmp/.git* .
-rm -rf tmp
+#cd /wks
+#git clone -b feature/refactor https://github.com/Gethi/discogs-client.git tmp
+#mv tmp/* tmp/.git* .
+#rm -rf tmp
 
-#tmp
-mv discogs_20190901_releases-exc.xml.gz data/XML/
+##tmp
+#mv discogs_20190901_releases-exc.xml.gz data/XML/
 
-yarn install
+#yarn install
 
-gzip -d data/XML/discogs_20190901_releases-exc.xml.gz
-cd tools
-./xml_split -s1Mb ../data/XML/discogs_20190901_releases-exc.xml
-cd ..
-ls -l data/XML
+#gzip -d data/XML/discogs_20190901_releases-exc.xml.gz
+#cd tools
+#./xml_split -s1Mb ../data/XML/discogs_20190901_releases-exc.xml
+#cd ..
+#ls -l data/XML
 
-node index.js
+#node index.js
 
-ls -l data/JSON
+#ls -l data/JSON
