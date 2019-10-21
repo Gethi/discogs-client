667857ad8a49b803e93d727715100e776ad304bc
Gethi <bigard.antoine@gmail.com>
2019-10-21 14:15:05 -0500

    wip

--- setup.sh
+++ setup.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 #set -xv
 
-yum -y install gcc-c++ make git wget epel-release
+yum -y install gcc-c++ make git wget
 curl -sL https://rpm.nodesource.com/setup_10.x | bash -
 yum -y install nodejs
 curl -sL https://dl.yarnpkg.com/rpm/yarn.repo | tee /etc/yum.repos.d/yarn.repo
 rpm --import https://dl.yarnpkg.com/rpm/pubkey.gpg
 yum -y install yarn
-yum -y install aria2
+
+rpm -Uvh http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.i686.rpm
+yum --enablerepo=rpmforge -y install aria2
 
 cd /scratch
 git clone -b feature/refactor https://github.com/Gethi/discogs-client.git tmp
